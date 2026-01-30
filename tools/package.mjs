4bbc4ad7992be89ad5ed4fde6f25db722bc0fe05
Egor Kushnarev <ekushnarev.work@yandex.ru>
2026-01-30 21:47:29 +0300

    pack helping tool

---
+++ tools/package.mjs
@@ -0,0 +1,60 @@
+
+import { readFileSync, writeFileSync, copyFileSync } from 'fs';
+import { join } from 'path';
+
+import { exit } from 'process';
+
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs';
+
+function getArgs() {
+	const argv = yargs(hideBin(process.argv))
+		.version(false)
+		.option('version', { demand: true, alias: 'v', type: 'string', default: '0.0.0', description: 'Version' })
+		.option('output', { demand: false, alias: 'o', default: 'dist', description: 'Output directory' })
+		.option('root', { demand: false, alias: 'r', default: '.', description: 'Root directory' })
+		.argv;
+
+	return argv;
+}
+
+function generatePackage() {
+	const packageMetadata = (version, dependencies) => ({
+		name: 'node-events-batcher',
+		version,
+		author: 'Egor Kushnarev',
+		description: 'Universal events batcher for NodeJS',
+		license: 'MIT',
+		keywords: [
+			'events-batcher',
+			'batcher',
+			'nodejs'
+		],
+		homepage: 'https://github.com/origin-yaropolk/node-events-batcher',
+		repository: {
+			type: 'git',
+			url: 'https://github.com/origin-yaropolk/node-events-batcher',
+		},
+		exports: './index.js',
+		dependencies,
+	});
+
+	const packageJson = packageMetadata(getArgs().version, JSON.parse(readFileSync(join(getArgs().root, 'package.json'))).dependencies);
+
+	writeFileSync(join(getArgs().output, 'package.json'), JSON.stringify(packageJson, null, '\t'));
+}
+
+function main() {
+	try {
+		generatePackage();
+		copyFileSync(join(getArgs().root, 'LICENSE'), join(getArgs().output, 'LICENSE'));
+		copyFileSync(join(getArgs().root, 'README.md'), join(getArgs().output, 'README.md'));
+		console.log('Successful compiled.');
+	} catch (err) {
+		console.error('FAIL:');
+		console.error(err);
+		exit(1);
+	}
+}
+
+main();
